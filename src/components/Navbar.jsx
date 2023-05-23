215ecc087a3aed2c73c5cbff0794d47cb71fe5c2
Pete Laptop <pmugasa78@gmail.com>
2023-05-23 18:17:33 +0200

    fixed the dropdown on mobile so that when the profile picture is clicked it appears

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ function Navbar() {
         </Link>
         <div className="ml-auto flex items-center space-x-2">
           <img
+            onClick={() => setShowDropdown(!showDropdown)}
             src="https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fGNhcnRvb24lMjBjaGFyYWN0ZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
             className="w-8 h-8 rounded-md"
           />
@@ -29,7 +30,7 @@ function Navbar() {
       </nav>
       {showDropdown && (
         <div
-          className="absolute z-30 sm:top-16 sm:right-16 md:right-20 lg:right-36 bg-[#fafafb]"
+          className="absolute z-30 right-2 sm:top-16 sm:right-16 md:right-20 lg:right-36 bg-[#fafafb]"
           onMouseLeave={() => setShowDropdown(false)}
         >
           <Dropdown />
